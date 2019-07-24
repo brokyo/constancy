88a1017275eccc2272f44472a898a05c9c111f2c
brokyo <alexcarusillo@gmail.com>
2019-07-24 15:40:23 -0400

    popup styling improvements

--- popup.js
+++ popup.js
@@ -27,10 +27,9 @@ function startSession(event){
 }
 
 function endSession() {
-	console.log('popup - end session')
-
+	
 	chrome.runtime.sendMessage({control: 'endSession'}, response => {
-		console.log('end session')
+			window.close()
 	})
 }
 
